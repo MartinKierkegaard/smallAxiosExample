a150ce7cc2a3787a910c189adab3fb12a010e009
Martin Kierkegaard <MARK@zealand.dk>
2024-11-12 09:22:06 +0100

    lille eksempel på get request med axios

--- js/main.js
+++ js/main.js
@@ -1,11 +1,57 @@
+const baseUrl = "http://localhost:5180/api/cars1"
+
 const app = Vue.createApp({
     data() {
         return {
             intro: 'Welcome to my Vue template',
+            carlist: [],
         }
     },
     methods: {
         myMethod(){
+
+        },
+        GetAllCars(){
+
+            console.log("er i GetAllCArs")
+
+            axios.get(baseUrl)
+            .then(response => {
+                this.carlist = response.data
+                console.log(carlist)
+            })
+            .catch(
+                error => {
+                    console.log("er i catch")
+                    console.log(error.response)
+
+                })
+
+               
+            //     axios.get(baseUrl)
+            //     .then(
+            //         function(response) {
+            //         console.log(response)
+            //     })
+            //     .catch(
+            //         function(error) {
+            //             console.log(response)
+            //         }
+            //     ) 
+
+            // axios.get(baseUrl)
+            // .then(
+            //     response => {
+            //     console.log(response)
+            // })
+            // .catch(
+            //     error =>{
+            //         console.log(error)
+            //     }
+            // )
+
+
+
 
         },
     },
